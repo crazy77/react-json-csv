3320e5ab380229cbca19a8e2d779adac06f62e81
fanta <crazy77d@nate.com>
2021-09-17 10:42:49 +0900

    add "\ufeff" + on making blob

--- src/helpers/saveCsv.js
+++ src/helpers/saveCsv.js
@@ -3,13 +3,10 @@ import { saveAs } from "file-saver";
 import { BLOB_TYPE_TEXT, BLOB_CHARSET_UTF8 } from "../constants";
 
 export default ({ data, fileformat, filename }) => {
-  const blob = new Blob(
-    [data],
-    {
-      type: BLOB_TYPE_TEXT,
-      charset: BLOB_CHARSET_UTF8
-    }
-  );
+	const blob = new Blob(["\ufeff" + data], {
+		type: BLOB_TYPE_TEXT,
+		charset: BLOB_CHARSET_UTF8,
+	});
 
-  saveAs(blob, [filename + "." + fileformat]);
+	saveAs(blob, [filename + "." + fileformat]);
 };
